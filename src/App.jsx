ea288c2464377d8422a013ea7cd0d17a28361266
Connor Pink <77735965+connorpink@users.noreply.github.com>
2024-07-18 17:24:01 -0400

    profile page
    page basic functionality implimented, needs more styling. users now  also have an email

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,27 @@ import './App.css'
 
 function App() {
 
+  async function get(request) {
+    try {
+      const response = await fetch(request);
+      const result = await response.json();
+      console.log("Success:", result);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+
+  // create a fetch request with a method, url, and body
+  const request1 = new Request("https://loki.trentu.ca/~connorpink/3430/assn/cois-3430-2024su-a2-BigBeill/api/movies", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+    }
+  });
+
+  get(request1);
+
+
   return (
     <>
 
