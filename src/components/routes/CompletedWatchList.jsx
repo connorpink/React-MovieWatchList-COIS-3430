8a955341e5366a8fb4b5a0942dfb3f65f51d8c8d
Connor Pink <77735965+connorpink@users.noreply.github.com>
2024-07-22 20:43:03 -0400

    completed watch list page added

--- src/components/routes/CompletedWatchList.jsx
+++ src/components/routes/CompletedWatchList.jsx
@@ -1,17 +1,47 @@
 import { useEffect, useState } from "react";
 import NavBar from "../NavBar";
-import MovieGrid from '../MovieGrid';
+import CompletedWatchListCard from '../CompletedWatchListCard';
 
 function CompletedWatchList() {
 
-    const [movieData, setMovieData] = useState([])
+    const [completedWatchListData, setCompletedWatchListData] = useState([])
     const [error, setError] = useState(null);
 
+    async function getWatchList() {
+        try {
+            const getRequest = {
+                method: 'GET',
+                headers: { "X-API-KEY": 'test' }
+            }
+
+            const response = await fetch(new Request('https://loki.trentu.ca/~connorpink/3430/assn/cois-3430-2024su-a2-BigBeill/api/completedwatchlist/entries/', getRequest));
+            if (!response.ok) {
+                setError({ message: "HTTP error! Status code is: " + response.status });
+                return;
+            }
+            const watchList = await response.json();
+
+            const movieList = await Promise.all(watchList.map(async element => {
+                const movieResponse = await fetch(`https://loki.trentu.ca/~connorpink/3430/assn/cois-3430-2024su-a2-BigBeill/api/movies/${element.movieID}`);
+                if (!movieResponse.ok) {
+                    setError({ message: "HTTP error! Status code is: " + movieResponse.status });
+                    return null;
+                }
+                const movieData = await movieResponse.json();
+                return {
+                    watchData: element,
+                    movieData: movieData[0]
+                };
+            }));
+
+            setCompletedWatchListData(movieList.filter(Boolean))
+        } catch (error) {
+            setError(error);
+        }
+    }
+
     useEffect(() => {
-        fetch('https://loki.trentu.ca/~connorpink/3430/assn/cois-3430-2024su-a2-BigBeill/api/movies')
-            .then(response => response.json())
-            .then(data => setMovieData(data))
-            .catch(error => setError(error));
+        getWatchList()
     }, []);
 
     return (
@@ -20,19 +50,20 @@ function CompletedWatchList() {
                 <NavBar />
             </header>
             <main>
-                <h1>Completed Watch List</h1>
-
                 {error ? (
                     <p>Error: {error.message}</p>
+                ) : completedWatchListData === null ? (
+                    <p>Loading...</p>
                 ) : (
                     <div>
-                        <MovieGrid movies={movieData} />
+                        {completedWatchListData.map((entry, index) => (
+                            <CompletedWatchListCard key={index} entry={entry.watchData} movie={entry.movieData} />
+                        ))}
                     </div>
                 )}
-
             </main>
         </>
     );
-};
+}
 
 export default CompletedWatchList;
