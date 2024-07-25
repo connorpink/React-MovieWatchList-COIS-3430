27bfb7a4fb48dd3f5f96e91ec3cf943cd0e1bb46
Connor Pink <77735965+connorpink@users.noreply.github.com>
2024-07-24 21:35:46 -0400

    fixed quickadd tooltip

--- src/components/routes/Movie.jsx
+++ src/components/routes/Movie.jsx
@@ -6,10 +6,10 @@ import { useLocation } from 'react-router-dom';
 function Movie() {
   const location = useLocation();
   const movie = location.state;
-
-  const params = useParams()
+  const params = useParams();
 
   const [error, setError] = useState(null);
+  const [successMessage, setSuccessMessage] = useState(null);
 
   //define async fetch function called quickAdd
   const quickAdd = async () => {
@@ -30,11 +30,12 @@ function Movie() {
       }
       const putResponse = await response.json();
 
-      return putResponse;
+      setSuccessMessage(`${movie.title} added to watch list`);
     } catch (error) {
       setError(error);
     }
   }
+
   return (
     <>
       <header>
@@ -43,10 +44,11 @@ function Movie() {
       <main>
         {/* display the title, release_date, vote_average, vote_count, runtime and description of a movie. */}
         <div className="movie-container">
+
           {movie && (
             <>
               <h1>{movie.title}</h1>
-              <p><span>movie ID:</span> {params.movieId}</p>
+              <p><span>Movie ID:</span> {params.movieId}</p>
               <p><span>Release Date:</span> {movie.release_date}</p>
               <p><span>Vote Average:</span> {movie.vote_average}</p>
               <p><span>Vote Count:</span> {movie.vote_count}</p>
@@ -55,14 +57,16 @@ function Movie() {
               <button onClick={quickAdd}>quick add to watch list</button>
               {error ? (
                 <span>Error: {error.message}</span>
+              ) : successMessage ? (
+                <span>{successMessage}</span>
               ) : (
-                <span>{movie.title} added to watch list</span>
+                <span />
               )}
             </>
           )}
 
         </div>
-      </main>
+      </main >
     </>
   );
 }
